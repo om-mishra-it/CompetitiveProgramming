a3b79cdf7dfe369d3d10af2e98da544f52a8202c
Om Mishra <ommishra32868@gmail.com>
2022-07-26 22:42:59 +0530

    Binary search recursively and iteratively

--- Base Code/BinarySearch.cpp
+++ Base Code/BinarySearch.cpp
@@ -8,3 +8,11 @@ int binarySearch(vector<int>& arr, int s, int e, int k) {
     }
     return -1;
 }
+
+int binarySearch(vector<int>& arr, int s, int e, int k) {
+    if(e < s) return -1;
+    int mid = s + (e-s) / 2;
+    if(arr[mid] == k) return mid;
+    if(arr[mid] < k) binarySearch(arr, mid + 1, e, k);
+    else binarySearch(arr, s, mid - 1, k);
+}
